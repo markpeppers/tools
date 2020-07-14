984c5d88155ac6868073231128584b6d66729d8b
Mark Peppers <markpeppers@MacBook-Pro.local>
2020-07-14 15:35:20 -0400

    Add setup scripts

---
+++ setup.sh
@@ -0,0 +1,107 @@
+#!/usr/bin/env bash
+# 
+# Bootstrap script for setting up a new OSX machine
+# 
+# This should be idempotent so it can be run multiple times.
+#
+# Some apps don't have a cask and so still need to be installed by hand. These
+# include:
+#
+# - Twitter (app store)
+# - Postgres.app (http://postgresapp.com/)
+#
+# Notes:
+#
+# - If installing full Xcode, it's better to install that first from the app
+#   store before running the bootstrap script. Otherwise, Homebrew can't access
+#   the Xcode libraries as the agreement hasn't been accepted yet.
+#
+# Reading:
+#
+# - http://lapwinglabs.com/blog/hacker-guide-to-setting-up-your-mac #SPAM
+# - https://gist.github.com/MatthewMueller/e22d9840f9ea2fee4716
+# - https://news.ycombinator.com/item?id=8402079
+# - http://notes.jerzygangi.com/the-best-pgp-tutorial-for-mac-os-x-ever/
+
+if [ -z "$DIRENV_DIR" ]; then
+    echo "ERROR: Please run this command from inside your dev directory."
+    exit 1
+fi
+
+echo "Starting bootstrapping"
+
+# Set working dir to script dir
+WORKING_DIR="${0%/*}"
+
+# Check for Homebrew, install if we don't have it
+if test ! $(which brew); then
+    echo "Installing homebrew..."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update homebrew recipes
+brew update
+
+# Install GNU core utilities (those that come with OS X are outdated)
+# brew tap homebrew/dupes
+# brew install coreutils
+# brew install gnu-sed --with-default-names
+# brew install gnu-tar --with-default-names
+# brew install gnu-indent --with-default-names
+# brew install gnu-which --with-default-names
+# brew install gnu-grep --with-default-names
+
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+# brew install findutils
+
+# Install Bash 4
+# brew install bash
+
+PACKAGES=(
+    ack
+    autoconf
+    automake
+    boot2docker
+    direnv
+    ffmpeg
+    fortune
+    gettext
+    gifsicle
+    git
+    graphviz
+    hub
+    imagemagick
+    jq
+    libjpeg
+    libmemcached 
+    lynx
+    markdown
+    memcached
+    mercurial
+    npm
+    pkg-config
+    postgresql
+    python
+    python3
+    pypy
+    rabbitmq
+    rename
+    ssh-copy-id
+    terminal-notifier
+    the_silver_searcher
+    tmux
+    tree
+    vim
+    wget
+)
+
+echo "Installing packages..."
+brew install ${PACKAGES[@]}
+
+echo "Cleaning up..."
+brew cleanup
+
+echo "Installing Go..."
+$WORKING_DIR/goinstall.sh
+
+echo "Bootstrapping complete"
